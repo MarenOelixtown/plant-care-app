0c78e0c5ed6a7fe66ee17d65d6d971b32b989ce7
Sabeen Javaid <khannssabeen@gmail.com>
2024-07-04 15:32:31 +0200

    add delete confirmation

--- components/ButtonDeletePlant.js
+++ components/ButtonDeletePlant.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import trash from "../public/trash.png";
 import Image from "next/image";
+import { useState } from "react";
 
 const StyledButton = styled.button`
   border: 2px solid black;
@@ -20,13 +21,47 @@ const StyledImage = styled(Image)`
   height: 25px;
 `;
 
+const ConfirmationDialog = styled.div`
+  position: absolute;
+  bottom: 50px;
+  right: 10px;
+  background: white;
+  border: 2px solid black;
+  padding: 10px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
+  z-index: 10;
+`;
+
+const DialogButton = styled.button`
+  margin: 5px;
+`;
+
 export default function ButtonDeletePlant({ OnDeletePlant, id }) {
+  const [isConfirming, setIsConfirming] = useState(false);
+
+  const handleDeleteClick = () => {
+    setIsConfirming(true);
+  };
+
+  const handleConfirmDelete = () => {
+    OnDeletePlant(id);
+    setIsConfirming(false);
+  };
+
+  const handleCancelDelete = () => {
+    setIsConfirming(false);
+  };
+
   return (
     <>
-      <StyledButton
-        title="Delete from My Plants"
-        onClick={() => OnDeletePlant(id)}
-      >
+      {isConfirming && (
+        <ConfirmationDialog>
+          <p>Are you sure you want to delete this plant?</p>
+          <DialogButton onClick={handleConfirmDelete}>Yes</DialogButton>
+          <DialogButton onClick={handleCancelDelete}>No</DialogButton>
+        </ConfirmationDialog>
+      )}
+      <StyledButton title="Delete from My Plants" onClick={handleDeleteClick}>
         <StyledImage src={trash} alt="DeleteMyPlant" />
       </StyledButton>
     </>
